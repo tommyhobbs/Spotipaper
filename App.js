74e8ad165a968f2108c03564454fa7fa317c399f
Tom Hobbs <tommyhobbs@hotmail.com>
2017-11-23 18:32:47 +0000

    working google link

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Linking} from 'react-native';
 
 export default class App extends React.Component {
+
   render() {
+    const url = 'http://google.com';
+
+    const login = () => {
+      console.log('Login clicked!');
+      Linking.canOpenURL(url).then(supported => {
+        if (!supported) {
+          console.log('Can\'t handle url: ' + url);
+        } else {
+          return Linking.openURL(url);
+        }
+      }).catch(err => console.error('An error occurred', err));
+    }
+
     return (
       <View style={styles.container}>
-        <Text>Welcome to Spotipaper please login below</Text>
+        <Text>Welcome to Spotipaper please login to Spotify below</Text>
         <Button
-          onPress={console.log('hello')}
+          onPress={login}
           title="Login"
           color="#841584"
           accessibilityLabel="Open window to Login to Spotify"/>
