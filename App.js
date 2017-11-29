62eeb84aec225f14f55278bc4a6e4cc8d24d64d9
Tom Hobbs <Tom.Hobbs@uk.ibm.com>
2017-11-29 17:13:34 +0000

    SUCCESS! coe retrieved :D

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, Button, Linking } from 'react-native';
 import { AuthSession } from 'expo';
 
-const FB_APP_ID = '243b3ba29bd44d0dbf1f70d1a82ebc20';
+const CLIENT_ID = '1e03fbd440fe4252a70719f1950c59c3';
 const scopes = ['user-top-read'];
 
 export default class App extends React.Component {
@@ -26,11 +26,12 @@ export default class App extends React.Component {
     console.log(redirectUrl);
     let result = await AuthSession.startAsync({
       authUrl:
-      `https://accounts.spotify.com/authorize?response_type=token` +
-      `&client_id=${FB_APP_ID}` +
+      `https://accounts.spotify.com/authorize?response_type=code` +
+      `&client_id=${CLIENT_ID}` +
       '&scope=' + encodeURIComponent(scopes.join(' ')) +
       `&redirect_uri=${encodeURIComponent(redirectUrl)}`,
     });
+    console.log(result);
     this.setState({ result });
   };
 }
