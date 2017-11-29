13d5beb2f45fb83dae7a56e24ccacea4e9e0dee6
Tom Hobbs <Tom.Hobbs@uk.ibm.com>
2017-11-29 11:52:16 +0000

    AuthSession attempt

--- App.js
+++ App.js
@@ -1,73 +1,38 @@
 import React from 'react';
-import { AsyncStorage, StyleSheet, Text, View, Button, Linking } from 'react-native';
+import { StyleSheet, Text, View, Button, Linking } from 'react-native';
+import { AuthSession } from 'expo';
+
+const FB_APP_ID = '243b3ba29bd44d0dbf1f70d1a82ebc20';
+const scopes = ['user-top-read'];
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.login = this.login.bind(this);
-    this.windowMessage = this.windowMessage.bind(this);
-    this.CLIENT_ID = '243b3ba29bd44d0dbf1f70d1a82ebc20';
-    this.REDIRECT_URI = 'http://localhost:3000/callback.html';
-    this.scopes = ['user-top-read'];
-    this.url = 'https://accounts.spotify.com/authorize?client_id=' + this.CLIENT_ID +
-      '&redirect_uri=' + encodeURIComponent(this.REDIRECT_URI) +
-      '&scope=' + encodeURIComponent(this.scopes.join(' ')) +
-      '&response_type=token';
-  }
-
-  componentDidMount() {
-    console.log('componentDidMount');
-    Linking.addEventListener('url', this._handleOpenURL);
-  }
-
-  componentWillUnmount() {
-    console.log('componentWillUnmount');
-    Linking.removeEventListener('url', this._handleOpenURL);
-  }
-
-  _handleOpenURL(event) {
-    console.log(event);
-    console.log(event.url);
-  }
-
-  login() {
-    console.log('Login clicked!');
-    Linking.canOpenURL(this.url).then((supported) => {
-      if (!supported) {
-        console.log('Can\'t handle url: ' + this.url);
-      } else {
-        return Linking.openURL(this.url);
-      }
-    }).catch(err => console.error('An error occurred', err));
-  }
-
-  windowMessage(e) {
-    if (typeof e.data === 'string') {
-      const data = JSON.parse(e.data);
-      if (data.hasOwnProperty('access_token')) {
-        AsyncStorage.setItem('Spotipaper.accessToken', data.access_token);
-      }
-      if (data.hasOwnProperty('expires_in')) {
-        const now = new Date();
-        const expires = new Date(now.getTime() + data.expires_in * 1000);
-        AsyncStorage.setItem('Spotipaper.expires', expires);
-      }
-      this.setState({loggedIn: true});
-    }
-  }
+  state = {
+    result: null,
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Welcome to Spotipaper please login to Spotify below</Text>
-        <Button
-          onPress={this.login}
-          title="Login"
-          color="#841584"
-          accessibilityLabel="Open window to Login to Spotify"/>
+        <Button title="Open FB Auth" onPress={this._handlePressAsync} />
+        {this.state.result ? (
+          <Text>{JSON.stringify(this.state.result)}</Text>
+        ) : null}
       </View>
     );
   }
+
+  _handlePressAsync = async () => {
+    let redirectUrl = AuthSession.getRedirectUrl();
+    console.log(redirectUrl);
+    let result = await AuthSession.startAsync({
+      authUrl:
+      `https://accounts.spotify.com/authorize?response_type=token` +
+      `&client_id=${FB_APP_ID}` +
+      '&scope=' + encodeURIComponent(scopes.join(' ')) +
+      `&redirect_uri=${encodeURIComponent(redirectUrl)}`,
+    });
+    this.setState({ result });
+  };
 }
 
 const styles = StyleSheet.create({
