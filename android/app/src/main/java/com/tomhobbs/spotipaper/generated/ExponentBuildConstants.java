4326fecd04c641e3a573dd106f6b7d2488686ad1
Tom Hobbs <Tom.Hobbs@uk.ibm.com>
2018-01-20 20:18:35 +0000

    working build

--- android/app/src/main/java/com/tomhobbs/spotipaper/generated/ExponentBuildConstants.java
+++ android/app/src/main/java/com/tomhobbs/spotipaper/generated/ExponentBuildConstants.java
@@ -3,6 +3,6 @@ package com.tomhobbs.spotipaper.generated;
 // This file is auto-generated. Please don't rename!
 public class ExponentBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "exp62c6ab88470c4310996780a6bc58827f://dn-zbq.tommyhobbs.spotipaper.exp.direct:80";
 
 }
