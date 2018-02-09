550f4d1fbbcfb9c3fdf53ecc1fe91a050c5c2fb9
Tom Hobbs <Tom.Hobbs@uk.ibm.com>
2018-02-09 16:28:50 +0000

    attempt at stripping permissions and adding required grade config

--- android/app/src/main/java/com/tomhobbs/spotipaper/generated/ExponentBuildConstants.java
+++ android/app/src/main/java/com/tomhobbs/spotipaper/generated/ExponentBuildConstants.java
@@ -3,6 +3,6 @@ package com.tomhobbs.spotipaper.generated;
 // This file is auto-generated. Please don't rename!
 public class ExponentBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "exp250a7eef0c884f60adbcfb2e524a84af://dn-zbq.tommyhobbs.spotipaper.exp.direct:80";
 
 }
